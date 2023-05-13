d3dd966ed9133e10a93cf5f9a49ad0437f15895b
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2023-05-13 17:36:59 -0400

    Enable sr-iov dkms module

---
+++ scripts/pre/sr_iov_enabling.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# FOLLOWING INSTRUCTIONS FROM:
+# https://www.michaelstinkerings.org/gpu-virtualization-with-intel-12th-gen-igpu-uhd-730/
+
+# Your code goes here.
+rpm-ostree install dkms sysfsutils -y
+echo "devices/pci0000:00/0000:00:02.0/sriov_numvfs = 7" > /etc/sysfs.conf
+
+#Part 3.1 - Installing the dkms module
+cd /usr/src/
+wget https://mirrors.apqa.cn/d/proxmox-edge/intel_gpu_sriov/i915-sriov-dkms-6.1.zip
+unzip i915-sriov-dkms-6.1.zip
+cd i915-sriov-dkms-6.1/
+dkms add .
+dkms install i915-sriov-dkms/6.1
