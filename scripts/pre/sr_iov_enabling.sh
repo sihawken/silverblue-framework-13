7d0082ce278f56cda3e78a85ba0d69b3bfd4d9b9
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2023-05-13 20:21:44 -0400

    add binutils

--- scripts/pre/sr_iov_enabling.sh
+++ scripts/pre/sr_iov_enabling.sh
@@ -9,7 +9,7 @@ set -oue pipefail
 # https://www.michaelstinkerings.org/gpu-virtualization-with-intel-12th-gen-igpu-uhd-730/
 
 # Install prerequisites
-rpm-ostree install git make kernel-devel-$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')
+rpm-ostree install git make binutils kernel-devel-$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')
 echo "devices/pci0000:00/0000:00:02.0/sriov_numvfs = 7" > /etc/sysfs.conf
 
 #Part 3.1 - Compiling the module
@@ -17,3 +17,10 @@ cd /usr/src/
 git clone https://github.com/strongtz/i915-sriov-dkms i915-sriov-dkms-6.1
 cd i915-sriov-dkms-6.1
 make -C /lib/modules/$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')/build M=$(pwd) i915.ko
+
+# May not need to be deleted - TO REMOVE
+# rm -rf /lib/modules/6.2.14-300.fc38.x86_64/kernel/drivers/gpu/drm/i915/i915.ko.xz
+
+xz i915.ko
+mv i915.ko /lib/modules/$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')/extra/i915.ko
+depmod $(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')
